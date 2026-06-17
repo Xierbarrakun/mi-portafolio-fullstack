2bb40dff26a6097eacf3df809d71306917bf4f7b
Xavier Guanipa <xavieralfonzoguanipalara22@gmail.com>
2026-06-17 13:51:29 -0400

    Fix: Configurar ruteo estricto con vercel.json en la raiz

--- portafolio/vite.config.js
+++ portafolio/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Enlaza de forma estricta la raíz del servidor para evitar los errores 404
+  base: '/', 
 })
